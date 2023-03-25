df2e4877b3563d36cc26e985555167cff8c07ba9
jujubea22 <114043890+jujubea22@users.noreply.github.com>
2023-03-24 23:24:41 -0400

    Update user.js

--- models/user.js
+++ models/user.js
@@ -6,12 +6,19 @@ const userSchema = new Schema(
     username:{
         type: String,
         required: true,
-    
-    },
-    email:{
-        type:String,
+        trim: true
+        },
+    email: {
+        type: String,
         required: true,
-    }
-
-
+        trim: true,
+        match: [/.+@.+\..+/, "Please enter a valid e-mail address."],
+        },
 })
+userSchema.virtual("friendCount").get(function() {
+    return this.friends.length;
+})
+
+const User = model("User", userSchema);
+
+module.exports = User;
