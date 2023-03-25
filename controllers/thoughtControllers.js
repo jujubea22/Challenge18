bbbae12039dedd79d190a74855ead14252c23acc
jujubea22 <114043890+jujubea22@users.noreply.github.com>
2023-03-24 23:18:20 -0400

    Update thoughtControllers.js
    get thoughts, get by id and creating them added to code

--- controllers/thoughtControllers.js
+++ controllers/thoughtControllers.js
@@ -1,8 +1,36 @@
-updateThoughts({params, body}, res)
-{
-    Thoughts.findOneUpdate({ id: params.thoughtsId}, body,)
+const {Thought, User} = require('../models');
 
-}
+module.exports = {
+    // git thoughts
+    getThoughts(req, res) {
+        Thought.find({})
+        .then((thoughtData) => res.json(thoughtData))
+        .catch((err) => res.status(400).json(err));
+    },
+
+    // git by id
+    getThoughtWithId(req, res) {
+        Thought.findOne(
+            {_id: req.params.id}
+        )
+        .then((thoughtData) => res.json(thoughtData))
+        .catch((err) => res.status(400).json(err));
+    },
+       // creating thoughts
+    createThought(req, res) {
+        Thought.create(
+            req.body
+        )
+        .then(({_id}) => {
+            return User.findOneAndUpdate(
+                {_id: req.params.userId},
+                {$push: {thoughts: _id}},
+                {new: true}
+            );
+        })
+        .then((thoughtData) => res.json(thoughtData))
+        .catch((err) => res.status(400).json(err));
+    },
 
 deleteThoughts({params}, res){
     Thgouths.findOneDelete({ id: params.thoughtsId})
