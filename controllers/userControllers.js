9d23c4c35543bdadf382862c4c14618df28a013a
jujubea22 <114043890+jujubea22@users.noreply.github.com>
2023-03-24 23:21:55 -0400

    Update userControllers.js
    how to get users, get by id and deleting users

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,15 +1,29 @@
 const { User, Thought} = require('../models');
 
-const userController = {
-    getAllUsers(req, res){
+module.exports = {
+    // getting a users
+    getUsers(req, res) {
         User.find({})
-        .populate({
-            path: 'thoughts'
-        })
-        
-    }
-}
+        .then((userData) => res.json(userData))
+        .catch((err) => res.status(400).json(err));
+    },
 
+    // get users by id
+    getUserWithId(req, res) {
+        User.findOne(
+            {_id: req.params.id}
+        )
+        .then((userData) => res.json(userData))
+        .catch((err) => res.status(400).json(err));
+    },
 getUserById({ params }, res){
     
 }
+      // deleting a user
+    deleteUser(req, res) {
+        User.findOneAndDelete(
+            {_id: req.params.id}
+        )
+        .then((userData) => res.json(userData))
+        .catch((err) => res.status(400).json(err));
+    },
